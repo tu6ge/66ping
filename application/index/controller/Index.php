8608db9680c20777f8a422f6499aacfeb424cf55
Wang honglei <wanghonglei@yingxinhuitong.com>
2018-02-12 17:22:31 +0800

    控制器列提纲

--- application/index/controller/Index.php
+++ application/index/controller/Index.php
@@ -3,13 +3,79 @@ namespace app\index\controller;
 
 class Index
 {
+    /**
+     * 门店列表
+     */
     public function index()
     {
 
     }
 
-    public function hello($name = 'ThinkPHP5')
+    /**
+     * 机构详情页
+     * @param string $name
+     * @return string
+     */
+    public function mechanism()
     {
-        return 'hello,' . $name;
+
+    }
+
+    /**
+     * 机构的更多反馈信息
+     */
+    public function mechFeedback()
+    {
+
+    }
+
+    /**
+     * 一条反馈信息
+     * 包含反馈的回复信息
+     */
+    public function feedback()
+    {
+
+    }
+
+    /**
+     * 提交反馈
+     */
+    public function feedbackAdd()
+    {
+
+    }
+
+    /**
+     * 提交评论
+     */
+    public function commentAdd()
+    {
+
+    }
+
+    /**
+     * 赠送红包
+     */
+    public function sendMoney()
+    {
+
+    }
+
+    /**
+     * 个人信息页
+     * 包含余额等信息
+     */
+    public function user()
+    {
+
+    }
+
+    /**
+     * 提现申请页面
+     */
+    public function withdrawal()
+    {
+
     }
 }
